7c0be6a23dab76e3b3ac739d6dcab37f634b349b
stefano-damato <78957872+stefano-damato@users.noreply.github.com>
2021-11-23 17:38:06 +0100

    calculator class

---
+++ ScientificCalculator/src/scientificcalculator/Calculator.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package scientificcalculator;
+
+import java.util.Stack;
+
+/**
+ *
+ * @author stefa
+ */
+public class Calculator {
+    
+    private Stack<Complex> complexStack;
+    
+    public Calculator(){
+        complexStack = new Stack<>();
+    }
+}
