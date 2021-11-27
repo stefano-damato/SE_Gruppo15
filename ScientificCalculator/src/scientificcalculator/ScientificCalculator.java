808a4494b2691c0bf0ec5e1afa5e2f028f89fccb
stefano-damato <78957872+stefano-damato@users.noreply.github.com>
2021-11-27 16:21:43 +0100

    windows title

--- ScientificCalculator/src/scientificcalculator/ScientificCalculator.java
+++ ScientificCalculator/src/scientificcalculator/ScientificCalculator.java
@@ -23,6 +23,7 @@ public class ScientificCalculator extends Application {
         
         Scene scene = new Scene(root);
         
+        stage.setTitle("Scientifc Calculator");
         stage.setScene(scene);
         stage.show();
     }
