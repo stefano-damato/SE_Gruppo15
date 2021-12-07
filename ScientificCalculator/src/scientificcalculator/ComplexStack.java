624b0930ee6c91062eaef81057c7d4ffb1df1ac7
nicol <nicol@LAPTOP-N714OUIQ.mshome.net>
2021-12-07 18:33:01 +0100

    Class ComplexStack

--- ScientificCalculator/src/scientificcalculator/ComplexStack.java
+++ ScientificCalculator/src/scientificcalculator/ComplexStack.java
@@ -5,10 +5,232 @@
  */
 package scientificcalculator;
 
+import exceptions.LessOf2ElementsException;
+import exceptions.OperationFailedException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.EmptyStackException;
+import java.util.HashMap;
+import java.util.Stack;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author nicol
  */
 public class ComplexStack {
+    /*** Contains all the complex that will be inserted*/
+    private Stack<Complex> complexStack; 
+    /** */
+    private HashMap<String, String> invokeOperations;
     
+    /**
+     * Initialize complexStack
+     */
+    public ComplexStack() {
+        complexStack = new Stack<>();
+        invokeOperations = new HashMap<>();
+        invokeOperations.put("+", "add");
+        invokeOperations.put("-", "sub");
+        invokeOperations.put("*", "multiply");
+        invokeOperations.put("/", "divide");
+        invokeOperations.put("+-", "invert");
+        invokeOperations.put("sqrt", "square");
+        invokeOperations.put("drop", "drop");
+        invokeOperations.put("dup", "dup");
+        invokeOperations.put("over", "over");
+        invokeOperations.put("swap", "swap");
+        invokeOperations.put("clear", "clear");
+    }
+    
+    public Stack<Complex> getComplexStack() {
+        return complexStack;
+    }
+    
+    /**
+     * The method insert a complex number into the stack
+     * @param c {@code Complex}
+     */
+    public void insert(Complex c){
+        complexStack.push(c);
+    }
+    
+    /**
+     * The method returns the last element within the stack
+     * @return complexStack.lastElement
+     */
+    public Complex lastElement(){
+        return complexStack.lastElement();
+    }
+    
+    /** */
+    public int complexStackSize(){
+        return complexStack.size();
+    }
+    
+    /**
+     * The method removes the last two elements from the stack, 
+     * execute the <code>add</code> method on the first one
+     * and puts the sum on the stack
+     * @throws LessOf2ElementsException if there are less of two elements in the stack
+     */
+    public void add() throws LessOf2ElementsException{
+        if(complexStack.size()<2)
+            throw new LessOf2ElementsException();
+        Complex a = complexStack.pop();
+        Complex b = complexStack.pop();
+        
+        insert(a.add(b));
+    }
+    
+    /**
+     * The method removes the last two elements from the stack, 
+     * execute the <code>sub</code> method on the first one
+     * and puts the sub on the stack
+     * @throws LessOf2ElementsException if there are less of two elements in the stack
+     */
+    public void sub() throws LessOf2ElementsException{
+        if(complexStack.size()<2)
+            throw new LessOf2ElementsException();
+        Complex a = complexStack.pop();
+        Complex b = complexStack.pop();
+        
+        insert(a.sub(b));
+    }
+    
+    /**
+     * The method removes the last two elements from the stack,
+     * execute the <code>multiply</code> method on the first one
+     * and puts the multiplication on the stack
+     * @throws LessOf2ElementsException if there are less of two elements in the stack
+     */
+    public void multiply() throws LessOf2ElementsException{
+        if(complexStack.size()<2)
+            throw new LessOf2ElementsException();
+        Complex a = complexStack.pop();
+        Complex b = complexStack.pop();
+        
+        insert(a.multiply(b));
+    }
+    
+    /**
+     * The method removes the last two elements from the stack,
+     * execute the <code>divide</code> method on the first one
+     * and puts the division on the stack
+     * @throws LessOf2ElementsException if there are less of two elements in the stack
+     */
+    public void divide() throws LessOf2ElementsException{
+        if(complexStack.size()<2)
+            throw new LessOf2ElementsException();
+        Complex a = complexStack.pop();
+        Complex b = complexStack.pop();
+        
+        insert(a.divide(b));
+    }
+    
+    /**
+     * The method removes the last element from the stack, 
+     * execute the <code>square</code> method
+     * and puts the result on the stack
+     * @throws EmptyStackException if the stack is empty
+     */
+    public void square() throws EmptyStackException{
+        if(complexStack.size()<1)
+            throw new EmptyStackException();
+        Complex a = complexStack.pop();
+        
+        insert(a.square());
+    }
+    
+    /**
+     * The method removes the last element from the stack,
+     * execute the <code>invert</code> method
+     * puts the result on the stack
+     * @throws EmptyStackException if the stack is empty
+     */
+    public void invert()throws EmptyStackException{
+        if(complexStack.size()<1)
+            throw new EmptyStackException();
+        Complex a = complexStack.pop();
+        
+        insert(a.invert());
+    }
+    
+    /**
+     * The method empties the stack
+     * @throws EmptyStackException if the stack is empty
+     */
+    public void clear() throws EmptyStackException{
+        if (complexStack.size() < 1)
+            throw new EmptyStackException();
+        complexStack.clear();
+    }
+    
+    /**
+     * The method removes the last element from the stack
+     * @throws EmptyStackException if the stack is empty
+     * @return complexStack.pop() {@code Complex}
+     */
+    public Complex drop() throws EmptyStackException{
+        if (complexStack.size() < 1)
+            throw new EmptyStackException();
+        return complexStack.pop();
+    }
+   
+    /**
+     * The method duplicates the last element in the stack 
+     * @throws EmptyStackException if the stack is empty
+     */
+    public void dup() throws EmptyStackException{
+        if (complexStack.size() < 1)
+            throw new EmptyStackException();
+        Complex a = complexStack.lastElement();
+        
+        insert(a);
+    }
+    
+    /**
+     * The method swap the last two elements in the stack
+     * @throws LessOf2ElementsException if there are less of two elements in the stack
+     */
+    public void swap() throws LessOf2ElementsException{
+        if(complexStack.size()<2)
+            throw new LessOf2ElementsException();
+        Complex a = complexStack.pop();
+        Complex b = complexStack.pop();
+        
+        insert(a);
+        insert(b);
+    } 
+    /**
+     * The method inserts a copy of the second last element in the stack
+     * @throws LessOf2ElementsException if there are less of two elements in the stack
+     */
+    public void over()throws LessOf2ElementsException{
+        if(complexStack.size()<2)
+            throw new LessOf2ElementsException();
+        Complex a = complexStack.get(complexStack.size() - 2);
+        
+        insert(a);
+    }
+        
+    public void selectOperationToInvoke(String op) throws OperationFailedException{
+        Method m;
+        if (invokeOperations.containsKey(op)){
+            try {
+                m = Calculator.class.getDeclaredMethod(invokeOperations.get(op));
+                m.invoke(this);
+            } catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException ex) {
+                throw new OperationFailedException();
+            }
+        }
+        /*
+        else if(!operations.containName(op)){
+            insert(op);
+        }
+        else{
+            invokeOperation(op);
+        }*/
+    }
 }
